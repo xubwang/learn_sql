b4ba66a49ae5c4c5050a3d525a72bbaa6c66df52
Xubo <xubwang816@gmail.com>
2022-06-07 23:06:04 +0200

    q2

--- examples.sql
+++ examples.sql
@@ -1,4 +1,4 @@
- -- Q1: to get the second highest salary from the Employee table.
+ -- Q1: Write a SQL query to get the second highest salary from the Employee table.
 
 select ifnull((
   select distinct Salary
@@ -8,20 +8,12 @@ select ifnull((
   null)
 as SecondHighestSalary;
 
-----
-To limit the number of rows returned by a select statement, you use the LIMIT and OFFSET clauses.
-The following shows the syntax of LIMIT & OFFSET clauses:
-SELECT
-    column_list
-FROM
-    table1
-ORDER BY column_list
-LIMIT row_count OFFSET offset;
+-- Q2: Write a SQL query to rank scores. If there is a tie between two scores, both
+-- should have the same ranking. Note that after a tie, the next ranking number
+-- should be the next consecutive integer value. In other words, there should be no
+-- "holes" between ranks.
 
-In this syntax:
-The LIMIT row_count determines the number of rows (row_count) returned by the query.
-The OFFSET offset clause skips the offset rows before beginning to return the rows.
-
-The OFFSET clause is optional. If you omit it, the query will return the row_count rows from the first row returned by the SELECT clause.
-When you use the LIMIT clause, it is important to use an ORDER BY clause to ensure the order of rows in the result set.
-----
+select Score, (select count(distinct Score) from Scores where Score >= s.Score)
+as Rank
+from Scores as s
+order by Score desc;
